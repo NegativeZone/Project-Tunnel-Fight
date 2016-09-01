db0019fabb955dadbd878c5dfb7092b3e8ca05a1
NegativeZone <anuroop@bisaria.com>
2016-09-02 01:00:31 +0530

    Level System 2.0 with Player Movement
    Much more elegant, works so much better. Uses text files to store
    levels. Quite interesting.

--- Project Tunnel Fight/Assets/Models/Player.cs
+++ Project Tunnel Fight/Assets/Models/Player.cs
@@ -3,6 +3,36 @@ using System.Collections;
 
 public class Player {
 
+	int HP;
+	int damage;
 
+	int x;
+
+	public int X {
+		get {
+			return x;
+		}
+		set {
+			x = value;
+		}
+	}
+
+	int y;
+
+	public int Y {
+		get {
+			return y;
+		}
+		set {
+			y = value;
+		}
+	}
+
+	public Player(int x, int y){
+		this.x = x;
+		this.y = y;
+		this.HP = 10;
+		this.damage = 5;
+	}
 
 }
