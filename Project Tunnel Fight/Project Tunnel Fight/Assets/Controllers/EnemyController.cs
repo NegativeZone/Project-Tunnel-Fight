942e67d38fb31a6424b32f8c512c08b65e6d93d8
NegativeZone <anuroop@bisaria.com>
2016-09-02 02:41:08 +0530

    Basic Static Enemies
    Last commit for now. Added static enemies, and basic combat animations.

---
+++ Project Tunnel Fight/Project Tunnel Fight/Assets/Controllers/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	Enemy enemy;
+	public int x;
+	public int y;
+	public GameObject BaddieSprite;
+	public GameObject BaddieBlood;
+
+	// Use this for initialization
+	void Start () {
+		enemy = new Enemy (x, y);
+		BaddieSprite.transform.position = new Vector3(enemy.X, enemy.Y, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (enemy.HP != 0)
+			LevelController.Instance.GetTileAtWorldCoord (BaddieSprite.transform.position).Enemy = enemy;
+		if (enemy.HP == 0) {
+			Destroy (BaddieSprite);
+			BaddieSprite = Instantiate (BaddieBlood);
+			BaddieSprite.transform.position = new Vector3(enemy.X, enemy.Y, 0);
+			LevelController.Instance.GetTileAtWorldCoord (BaddieSprite.transform.position).Enemy = null;
+		}
+	}
+}
---
+++ Project Tunnel Fight/Project Tunnel Fight/Assets/Models/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy {
+
+	int hP;
+
+	public int HP {
+		get {
+			return hP;
+		}
+		set {
+			hP = value;
+		}
+	}
+
+	int damage;
+
+	int x;
+
+	public int X {
+		get {
+			return x;
+		}
+		set {
+			x = value;
+		}
+	}
+
+	int y;
+
+	public int Y {
+		get {
+			return y;
+		}
+		set {
+			y = value;
+		}
+	}
+
+	public Enemy(int x, int y){
+		this.x = x;
+		this.y = y;
+		this.hP = 10;
+		this.damage = 5;
+	}
+}
